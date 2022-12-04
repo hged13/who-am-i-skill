200362fc5421dea7f81264f5cd6bb030a5e1f781
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-04 12:42:17 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,19 +4,14 @@ from mycroft import MycroftSkill, intent_file_handler
 class WhoAmI(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-    
-    
-    def initialize(self):
-        answer = self.setcolor()
+        file = self.open('/home/pi/.config/mycroft/skills/NewUserCreation/wav.csv', 'r')
         
 
     @intent_file_handler('i.am.who.intent')
     def handle_i_am_who(self, message):
         self.speak_dialog('i.am.who')
     
-    def setcolor(self):
-        return true
-
+   
 def create_skill():
     return WhoAmI()
 
