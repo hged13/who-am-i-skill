ddfe7f43196fbe2b8b68ce337389271dcd75ab97
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-04 12:55:52 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,23 @@
 from mycroft import MycroftSkill, intent_file_handler
+import pandas as pd
 
 
 class WhoAmI(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        file = self.open('/home/pi/.config/mycroft/skills/NewUserCreation/wav.csv', 'r')
-        
 
     @intent_file_handler('i.am.who.intent')
     def handle_i_am_who(self, message):
         self.speak_dialog('i.am.who')
-    
-   
+        model = self.build_model()
+
+    def build_model(self):
+        file = self.open('/home/pi/.config/mycroft/skills/NewUserCreation/wav.csv', 'r')
+        df = pd.read_csv(file)
+        return df
+
+
+
+
 def create_skill():
     return WhoAmI()
-
