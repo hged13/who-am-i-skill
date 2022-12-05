2b77060f30f528b0ecd5351ba1aa5e103152ecbb
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-04 21:23:25 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
+import numpy as np
 from mycroft import MycroftSkill, intent_file_handler
 import pandas as pd
+import librosa
+import librosa.display
+from sklearn.neighbors import KNeighborsClassifier
+
+
 
 
 class WhoAmI(MycroftSkill):
@@ -12,11 +18,37 @@ class WhoAmI(MycroftSkill):
         model = self.build_model()
 
     def build_model(self):
-        file = open('/home/pi/.config/mycroft/skills/NewUserCreation/wav.csv', 'r')
+        file = self.open('/home/pi/.config/mycroft/skills/NewUserCreation/wav.csv', 'r')
         df = pd.read_csv(file)
-        return df
+        audio_data, sampling_rate = librosa.load(df.iloc[0][0])
+        mfccs = librosa.feature.mfcc(y=audio_data, sr=sampling_rate, n_mfcc=40)
+        extracted_features = []
+        for row in df.itertuples(name='file_name'):
+            file_name = row[1]
+            data = self.features_extractor(file_name)
+            speaker = row[2]
+            extracted_features.append([data, speaker])
 
+        extracted_features_df = pd.DataFrame(extracted_features, columns=['audio', 'speaker'])
 
+        X = np.array(extracted_features_df['audio'].tolist())
+        y = np.array(extracted_features_df['speaker'].tolist())
+
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+        dtc = KNeighborsClassifier(n_neighbors=4)
+        dtc.fit(X_train, y_train)
+
+        y_pred = dtc.predict(X_test)
+            
+
+    def features_extractor(self,file):
+        audio, sample_rate = librosa.load(file, res_type='kaiser_fast')
+        mfcss_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        mfccs_scaled_features = np.mean(mfcss_features.T, axis=0)
+
+        return mfccs_scaled_features
 
 
 def create_skill():
