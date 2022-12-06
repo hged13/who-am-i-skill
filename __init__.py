eeef2b7a144a08258522e6aa81ce89e7eb011f93
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-06 00:56:16 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@ from mycroft import MycroftSkill, intent_file_handler
 import pandas as pd
 import numba
 import numpy as np
-import numpy.core.multiarray
-import librosa
+import tensorflow as tf
 
 
 
