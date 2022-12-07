a2f83eb5a6e7a683cd63cadfbceea16de0c32087
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-07 16:32:45 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ class WhoAmI(MycroftSkill):
     def handle_i_am_who(self, message):
         self.speak_dialog('i.am.who')
         model = self.build_model()
-        pred = get_prediction_sample(model)
+        pred = self.get_prediction_sample(model)
         self.speak_dialog("YOU DID IT GIRL")
     
     def get_prediction_sample(self, model):
