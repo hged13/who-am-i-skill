d27baa82527a851d54b34d8839cd683424184326
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-13 23:19:47 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,7 +76,7 @@ class WhoAmI(MycroftSkill):
         name3 = str(name2)
         artist1 = self.df.loc[self.df['name'].eq(name3), 'artist']
         playlist1 = self.df.loc[self.df['name'].eq(name3), 'playlist']
-        artist2 = self.df.loc[self.df['name'].eq(name3), 'playlist2']
+        artist2 = self.df.loc[self.df['name'].eq(name3), 'artist2']
         self.artist = str(artist1[0])
         self.playlist = str(playlist1[0])
         self.artist2 = str(artist2[0])
